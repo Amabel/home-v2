8cb5c7ea89615e78418aebe351a2c204bb0a6b9d
Amabel <luoweibinb@gmail.com>
2022-09-17 19:11:23 +0900

    chore: add tag cloud

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,40 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import * as TagCloud from 'TagCloud';
+import { onMounted } from 'vue';
+
+let tags = [
+  { tagName: 'js', count: 5 },
+  { tagName: 'css', count: 9 },
+  { tagName: 'less', count: 13 },
+  { tagName: 'rest', count: 2 },
+];
+
+const container = '#tagcloud';
+const texts = [
+  '3D',
+  'TagCloud',
+  'JavaScript',
+  'CSS3',
+  'Animation',
+  'Interactive',
+  'Mouse',
+  'Rolling',
+  'Sphere',
+  '6KB',
+  'v2.x',
+];
+const options = {};
+
+onMounted(() => {
+  TagCloud(container, texts, options);
+});
 </script>
 
 <template>
+  <div id="tagcloud"></div>
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
@@ -17,6 +47,9 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style scoped>
+>>> .tagcloud--item {
+  color: blue;
+}
 .logo {
   height: 6em;
   padding: 1.5em;
