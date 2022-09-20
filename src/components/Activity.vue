58144eefd5805be4e26e4f550d81d17e49a35b77
Amabel <luoweibinb@gmail.com>
2022-09-20 15:41:39 +0900

    feat: add meta tags and seo

--- src/components/Activity.vue
+++ src/components/Activity.vue
@@ -9,10 +9,12 @@
         <div class="mt-8 md:mt-12 animate__animated animate__fadeInUp">
           <img
             src="https://n4f6tu2ms5.execute-api.ap-northeast-1.amazonaws.com/prod/api/stats?theme=tokyonight&hide_border=true&hide_bg=true"
+            alt="Amabel's GitHub Stats"
           />
           <img
             class="mt-8"
             src="https://n4f6tu2ms5.execute-api.ap-northeast-1.amazonaws.com/prod/api/wakatime?theme=tokyonight&hide_border=true&hide_bg=true"
+            alt="Amabel's Wakatime Stats"
           />
         </div>
       </div>
