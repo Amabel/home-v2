c3fadded2e268721d9b009ba8a01c509c8128b92
Amabel <luoweibinb@gmail.com>
2022-09-20 13:20:46 +0900

    feat: add bio

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,12 +3,29 @@
     class="main inline-flex items-center w-full h-[calc(100%-4rem)] md:h-screen"
   >
     <div class="flex flex-col p-12">
-      <RubberBandHeader text="About Amabel" />
-      <p
-        class="mt-4 md:mt-12 text-base md:text-lg animate__animated animate__fadeInUp"
+      <RubberBandHeader text="Bio" />
+      <div
+        class="mt-4 md:mt-12 text-sm md:text-lg animate__animated animate__fadeInUp"
       >
-        Fullstack engineer.
-      </p>
+        <div>
+          Weibin Luo, a full-stack engineer with 4 years' experience in software
+          development, including requirements analysis, architecture design,
+          programming, operations and maintenance.
+        </div>
+
+        <div class="mt-2 md:mt-4">
+          As a developer, he is expert in frontend and backend development,
+          cloud architecture, system availability and code maintainability. As a
+          team leader, he focues on project management, code review, knowledge
+          sharing and pair programming.
+        </div>
+
+        <div class="mt-2 md:mt-4">
+          Weibin Luo has a passion for cutting edge programming languages. In
+          his free time, he likes to contribute to OSS and build his own
+          projects.
+        </div>
+      </div>
     </div>
   </div>
 </template>
